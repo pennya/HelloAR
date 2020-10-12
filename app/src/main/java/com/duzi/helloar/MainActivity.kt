64bd5342002856517289c89b930b74a3577c3c12
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2020-10-13 03:32:56 +0900

    축구공 렌더링

--- app/src/main/java/com/duzi/helloar/MainActivity.kt
+++ app/src/main/java/com/duzi/helloar/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.duzi.helloar
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.BaseArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import java.util.function.Consumer
 import java.util.function.Function
 
@@ -12,26 +17,37 @@ import java.util.function.Function
 class MainActivity : AppCompatActivity() {
 
     lateinit var fragment: ArFragment
-    private var modelRenderable: ModelRenderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+        fragment.setOnTapArPlaneListener(BaseArFragment.OnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor : Anchor = hitResult.createAnchor()
 
-        ModelRenderable.builder()
-            .setSource(this, R.raw.beagle)
-            .build()
-            .thenAccept(Consumer { renderable: ModelRenderable ->
-                modelRenderable = renderable
-            })
-            .exceptionally(
-                Function<Throwable, Void?> { throwable: Throwable? ->
-                    Log.e(TAG, "Unable to load Renderable.", throwable)
-                    null
-                }
-            )
+            ModelRenderable.builder()
+                .setSource(this, Uri.parse("soccer.sfb"))
+                .build()
+                .thenAccept(Consumer { renderable: ModelRenderable ->
+                    addModelToScene(anchor, renderable)
+                })
+                .exceptionally(
+                    Function<Throwable, Void?> { throwable: Throwable? ->
+                        Log.e(TAG, "Unable to load Renderable.", throwable)
+                        null
+                    }
+                )
+        })
+    }
+
+    private fun addModelToScene(anchor: Anchor, it: ModelRenderable?) {
+        val anchorNode : AnchorNode = AnchorNode(anchor)
+        val transform : TransformableNode = TransformableNode(fragment.transformationSystem)
+        transform.setParent(anchorNode)
+        transform.renderable = it
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transform.select()
     }
 
     companion object {
