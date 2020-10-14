cd59e6b0a32f1f6e033056329d2c8334ad8ce29f
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2020-10-14 21:09:43 +0900

    메모장 + 텍스트 렌더링

--- app/src/main/java/com/duzi/helloar/MainActivity.kt
+++ app/src/main/java/com/duzi/helloar/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.duzi.helloar
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.FixedHeightViewSizer
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.BaseArFragment
 import com.google.ar.sceneform.ux.TransformableNode
-import java.util.function.Consumer
 import java.util.function.Function
 
 
@@ -22,16 +25,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val memoLayout = LayoutInflater.from(this).inflate(R.layout.memo_view, null)
+        val memoTitle = memoLayout.findViewById<TextView>(R.id.memoTitle)
+        val memoCard = memoLayout.findViewById<ImageView>(R.id.memoCard)
+
+        memoTitle.text = "ARCore Test!"
+
+
         fragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
         fragment.setOnTapArPlaneListener(BaseArFragment.OnTapArPlaneListener { hitResult, plane, motionEvent ->
             val anchor : Anchor = hitResult.createAnchor()
 
-            ModelRenderable.builder()
-                .setSource(this, Uri.parse("soccer.sfb"))
+            ViewRenderable.builder()
+                .setView(this, memoLayout)
+                .setSizer(FixedHeightViewSizer(0.20f))
                 .build()
-                .thenAccept(Consumer { renderable: ModelRenderable ->
+                .thenAccept { renderable ->
                     addModelToScene(anchor, renderable)
-                })
+                }
                 .exceptionally(
                     Function<Throwable, Void?> { throwable: Throwable? ->
                         Log.e(TAG, "Unable to load Renderable.", throwable)
@@ -41,11 +52,11 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun addModelToScene(anchor: Anchor, it: ModelRenderable?) {
+    private fun addModelToScene(anchor: Anchor, renderable: Renderable?) {
         val anchorNode : AnchorNode = AnchorNode(anchor)
         val transform : TransformableNode = TransformableNode(fragment.transformationSystem)
         transform.setParent(anchorNode)
-        transform.renderable = it
+        transform.renderable = renderable
         fragment.arSceneView.scene.addChild(anchorNode)
         transform.select()
     }
